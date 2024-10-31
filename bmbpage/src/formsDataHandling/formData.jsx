622aeadfeb5527a8483a22aea9e89b94556bd154
Zaheer Ahmad Safeer <Zaheer.Ahmed@bellmedex.com>
2024-10-31 11:16:50 -0700

    Homepage first form data has been sent to gmail.

---
+++ bmbpage/src/formsDataHandling/formData.jsx
@@ -0,0 +1,29 @@
+import emailjs from "@emailjs/browser";
+
+const homeForm1 = (details) => {
+    // initialize using your User ID saved in the .env file
+    emailjs.init('N6f4nCAkgbvNeqgu8');
+    emailjs
+        .send(
+            ("service_x9vnzkd"), // The service ID saved in the .env file
+            ("template_43ujbuj"), // The template ID also saved in the .env file
+            // Start of the variables defined in the template earlier
+            {
+                from_name: details.from_name,
+                to_email: details.to_email,
+                phone: details.phone,
+                state: details.state,
+            }
+            // End of the variables defined in the template earlier
+        )
+        .then((response) => {
+            // Debug statement on the console to show the function has been executed successfully
+            console.log(response);
+        })
+        .catch((error) => {
+            // Debug statement on the console to show the error that occured
+            console.log(error);
+        });
+};
+
+export { homeForm1 };
